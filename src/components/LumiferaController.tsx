869e43e2dc7248642500cff5256c3bb5b98543e1
Costyn van Dongen <costynvd@gmail.com>
2025-10-22 22:11:03 +0200

    fix: More margins on phone screens

--- src/components/LumiferaController.tsx
+++ src/components/LumiferaController.tsx
@@ -49,7 +49,7 @@ export function LumiferaController() {
     }
 
     return (
-        <div className="space-y-4 max-w-6xl mx-auto p-4">
+        <div className="space-y-4 max-w-6xl mx-auto p-2 md:p-4">
             {/* Main Card */}
             <Card>
                 <MainCardHeader
